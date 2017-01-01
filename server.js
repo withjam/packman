6759108fb00a21f760cd3b2211d95be895dad04f
Matt Pileggi <Matt.Pileggi@MarkLogic.com>
2017-01-01 12:58:39 -0500

    ui organization

--- server.js
+++ server.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+var multer = require('multer');
+var upload = multer(); // for parsing multipart/form-data
+var sassMiddleware = require('node-sass-middleware');
 
 var config = require('config');
-var logger = bunyan.createLogger(config.bunyan);
+var logger = require('bunyan').createLogger(config.bunyan);
+
+var path = require('path');
+var uiPath =  path.join(__dirname, 'ui');
+var staticPath = path.join(uiPath,'www');
+var templatePath = path.join(uiPath, 'lib');
 
 var mysql = require('mysql');
 
-var bwip = require('bwip');
+var bwip = require('bwip-js');
+
+// static bundles
+var glob = require('glob');
+var assets = require('assets-middleware');
+var packmanJs = {
+  src: function(destpath, callback) {
+    glob('./ui/lib/**/*.js', null, callback);
+  },
+  dest: './ui/www/script/packman.js',
+  serve: false
+}
+
+app.use(sassMiddleware({
+  src: path.join(uiPath,'styles'),
+  force: process.env.NODE_ENV === 'production',
+  prefix: '/styles',
+  includePaths: [
+    path.join(staticPath,'bower_components', 'bootstrap-sass', 'assets', 'stylesheets'),
+    path.join(staticPath,'bower_components', 'font-awesome', 'scss')
+  ]
+}));
+app.get('/scripts/packman.js', assets(packmanJs)); // pre-bundle the assets
+app.use(express.static(staticPath));
+app.use('/template', express.static(templatePath, { extensions: [ 'html', 'htm' ]}));
+
+// parse cookies
+app.use(cookieParser());
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// custom middleware for easier use
+app.use(function(req, res, next) {
+  //req.db = tvclient; // expose tvclient to all request handlers, abstract as db
+  req.logger = logger; // expose logger to all request handlers
+  next();
+});
+
+// fallback route for SPA
+var fallback = require('express-history-api-fallback');
+app.use(fallback('index.html', { root: staticPath }))
---
+++ ui/lib/packman.js
@@ -0,0 +1,31 @@
+(function(window, document) {
+
+  var app = angular.module('packman',['ngRoute']);
+
+  app
+    .controller('homeCtrl',HomeCtrl)
+    .config(PackmanConfig);
+
+
+  function HomeCtrl() {
+    var ctrl = this;
+
+    
+  }
+
+
+  PackmanConfig.$inject = ['$routeProvider','$locationProvider'];
+  function PackmanConfig($route,$location) {
+    $location.html5Mode(true);
+    $route
+      .when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeCtrl'
+      })
+      .otherwise('/');
+
+  }
+
+
+  
+})(window, document);
