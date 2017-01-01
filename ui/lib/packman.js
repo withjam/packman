dbdeb249b8bd3276a68efc19abb6af06d258ccba
Matt Pileggi <Matt.Pileggi@MarkLogic.com>
2017-01-01 14:55:35 -0500

    scan barcodes

--- ui/lib/packman.js
+++ ui/lib/packman.js
@@ -7,9 +7,51 @@
     .config(PackmanConfig);
 
 
-  function HomeCtrl() {
+
+  function HomeCtrl($scope) {
     var ctrl = this;
 
+    function codeDetected(data) {
+      console.log('code detected', data);
+      ctrl.codeFound = data.codeResult.code;
+      $scope.$apply();
+    }
+
+    function startScanner() {
+      Quagga.init({
+        locate: true,
+        inputStream : {
+          name : "Live",
+          type : "LiveStream",
+          target: '#quaggas-viewport',
+          constraints: {
+            width: '500',
+            height: '500'
+          }
+        },
+        decoder : {
+          readers : ["code_128_reader"]
+        }
+      }, function(err) {
+          if (err) {
+              console.log(err);
+              return
+          }
+          console.log("Initialization finished. Ready to start");
+          Quagga.start();
+      });
+      Quagga.onDetected(codeDetected);
+    }
+
+    ctrl.launchScanner = function() {
+      ctrl.scannerActive = true;
+      startScanner();
+    }
+
+    ctrl.endScanner = function() {
+      Quagga.stop();
+      ctrl.scannerActive = false;
+    }
     
   }
 
@@ -20,7 +62,7 @@
     $route
       .when('/', {
         templateUrl: 'templates/home.html',
-        controller: 'homeCtrl'
+        controller: 'homeCtrl as ctrl'
       })
       .otherwise('/');
 
